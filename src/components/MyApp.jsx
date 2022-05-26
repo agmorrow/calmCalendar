601838bec2fae85b63213c0f6e073781a256de3f
agmorrow <morrow.austin5@gmail.com>
2022-05-25 23:02:53 -0600

    fixed tailwind / centered div

--- src/components/MyApp.jsx
+++ src/components/MyApp.jsx
@@ -3,31 +3,36 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import Calm from "../assets/calm_logo.png";
 
-function MyApp() {
-  const [value, onChange] = useState(new Date());
-
+function MyApp () {
+  const[value, onChange] = useState(new Date ());
   return (
-    <div className="container p-4 flex items-center justify-center shadow-lg rounded-lg bg-white h-1/3 mb-auto">
-      <div>
-      <div>
-        <img src={Calm} alt="calm logo" className='max-w-[100px]' />
-      </div>
-      <h1>Customer support interview</h1>
-      <h2>with Calm GTM</h2>
-      <p>45 minutes</p>
-      <p>Google meeting will be created at booking</p>
-      <h2>About this meeting</h2>
-      <p>Thank you for scheduling your interview.
-        We look forward to you presentation on
-        Calm's proposals. Please bring any
-        questions you may have about the
-        position.
-      </p>
-      </div>
-      <div>
-      <Calendar onChange={onChange} value={value} />
+    <div className='w-full text-white my-24'>
+      
+
+      <div className='max-w-[1240px] mx-auto py-12'>
+
+        <div className='grid md:grid-cols-2'>
+
+          <div className='bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl relative'>
+            <div>
+              <img src={Calm} alt="calm logo" />
+            </div>
+            <p className='text-2xl py-8'>Customer support interview</p>
+            <p className='text-2xl py-8 text-slate-500'>with Calm GTM</p>
+            <p className='flex py-4'>45 minutes</p>
+            <p className='flex py-4'>Google meeting will be created at booking.</p>
+            <div className='text-2xl'>
+                <p className='flex py-4'>About this meeting:</p>
+                <p className='flex py-4'>Thank you for scheduling your interview. We look forward to your presentation on Calms proposals. Please bring any questions you may have about the position</p>
+            </div>
+          </div>
+          <div className='bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl relative'>
+           <Calendar onchange={onChange} value={value} />
+          </div>
+        </div>
       </div>
     </div>
+  
   );
 }
 export default MyApp;
